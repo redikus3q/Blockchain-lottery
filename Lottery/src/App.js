0433d4614030bef37df7fac585d3e01007a252f4
Alexandru-Daniel Tiganus <alexandu24@gmail.com>
2023-05-17 15:06:53 +0300

    separate abi from code

--- Lottery/src/App.js
+++ Lottery/src/App.js
@@ -2,84 +2,8 @@ import { ethers } from "ethers";
 import React, { useEffect, useState } from "react";
 import { FaMoneyBillAlt } from "react-icons/fa";
 import "./App.css";
+import contractABI from "./contractABI.js";
 
-const contractABI = [
-  { inputs: [], stateMutability: "nonpayable", type: "constructor" },
-  {
-    inputs: [{ internalType: "uint256", name: "lotteryId", type: "uint256" }],
-    name: "BuyTicket",
-    outputs: [],
-    stateMutability: "payable",
-    type: "function",
-  },
-  {
-    inputs: [
-      { internalType: "string", name: "name", type: "string" },
-      { internalType: "uint256", name: "startDate", type: "uint256" },
-    ],
-    name: "CreateLottery",
-    outputs: [],
-    stateMutability: "nonpayable",
-    type: "function",
-  },
-  {
-    inputs: [{ internalType: "uint256", name: "lotteryId", type: "uint256" }],
-    name: "FinishLottery",
-    outputs: [],
-    stateMutability: "nonpayable",
-    type: "function",
-  },
-  {
-    inputs: [],
-    name: "m_commision",
-    outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
-    stateMutability: "view",
-    type: "function",
-  },
-  {
-    inputs: [{ internalType: "uint256", name: "", type: "uint256" }],
-    name: "m_lotteries",
-    outputs: [
-      { internalType: "string", name: "name", type: "string" },
-      { internalType: "uint256", name: "startDate", type: "uint256" },
-      { internalType: "uint256", name: "totalPrize", type: "uint256" },
-      { internalType: "bool", name: "finalized", type: "bool" },
-    ],
-    stateMutability: "view",
-    type: "function",
-  },
-  {
-    inputs: [],
-    name: "m_lotteryId",
-    outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
-    stateMutability: "view",
-    type: "function",
-  },
-  {
-    inputs: [],
-    name: "m_owner",
-    outputs: [{ internalType: "address", name: "", type: "address" }],
-    stateMutability: "view",
-    type: "function",
-  },
-  {
-    inputs: [],
-    name: "m_ticketPrice",
-    outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
-    stateMutability: "view",
-    type: "function",
-  },
-  {
-    inputs: [
-      { internalType: "uint256", name: "", type: "uint256" },
-      { internalType: "uint256", name: "", type: "uint256" },
-    ],
-    name: "m_tickets",
-    outputs: [{ internalType: "address", name: "", type: "address" }],
-    stateMutability: "view",
-    type: "function",
-  },
-];
 const contractAddress = "0xA84D5DC2e6F3679aa546603F8d40F5624abF0020";
 const provider = new ethers.providers.Web3Provider(window.ethereum);
 const signer = provider.getSigner();
